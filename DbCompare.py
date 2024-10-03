e61e05227dc2ce062389b0e46debf700fffd4c8c
Mohamed Mansour <MansourMohamedAli1996@gmail.com>
2024-10-02 22:37:37 -0400

    Will automatically use first column for comparison. Must specify table name.

--- DbCompare.py
+++ DbCompare.py
@@ -25,13 +25,13 @@ def read_table(connection, table_name):
     return pd.read_sql(query, connection)
 
 # Function to compare two DataFrames for specific column differences but return all columns in the output
-def compare_column_differences(df1, df2, table_name, columns_to_compare, output_file, db1_name, db2_name):
+def compare_column_differences(df1, df2, table_name, output_file, db1_name, db2_name):
     # Add a source column to identify the origin of each row
     df1['source'] = 'db1'
     df2['source'] = 'db2'
 
     # Merge the two DataFrames based on the specified columns to compare
-    merge_on = columns_to_compare
+    merge_on = df1.columns[0]
     merged = pd.merge(df1, df2, on=merge_on, how='outer', indicator=True)
 
     # Separate out rows that differ between the two DataFrames
@@ -40,22 +40,18 @@ def compare_column_differences(df1, df2, table_name, columns_to_compare, output_
 
     headings_list = list()
     for heading in new_in_db1.columns.values:
-        # if heading == "f_ptid" or heading == "_merge":
-        if heading in columns_to_compare or heading == "_merge":
+        if heading in df1.columns[0] or heading == "_merge":
             headings_list.append(heading)
         else:
             headings_list.append(heading[:-2])
-            # headings_list.append(heading)
     new_in_db1.columns = headings_list
 
     headings_list = list()
     for heading in new_in_db2.columns.values:
-        # if heading == "f_ptid" or heading == "_merge":
-        if heading in columns_to_compare or heading == "_merge":
+        if heading in df1.columns[0] or heading == "_merge":
             headings_list.append(heading)
         else:
             headings_list.append(heading[:-2])
-            # headings_list.append(heading)
     new_in_db2.columns = headings_list
     
     # Write the results to the output file
@@ -74,10 +70,10 @@ def compare_column_differences(df1, df2, table_name, columns_to_compare, output_
         else:
             f.write(f"No new entries in table '{table_name}' found only in {db2_name}.\n\n")
     
-    if not new_in_db1.empty:
+    if not new_in_db1.empty and df1.columns[0] == 'f_ptid':
         create_add_file(new_in_db1, db1_name[:-4])
         
-    if not new_in_db2.empty:
+    if not new_in_db2.empty and df1.columns[0] == 'f_ptid':
         create_add_file(new_in_db2, db2_name[:-4])
 
 
@@ -85,20 +81,36 @@ def create_add_file(db, db_name):
         with open(f"{db_name}.add", 'w') as f:
             for row in db.itertuples(index=True, name='Pandas'):
                 f.write(f'add {row.f_ptid}\n')
-                f.write(f'.desc {row.f_brief}\n')
-                f.write(f'{row.f_ldes}\n')
-                f.write(f'.units {row.f_unit}\n')
+                try:
+                    f.write(f'.desc {row.f_brief}\n')
+                except AttributeError: # No Value
+                    f.write(f'.desc\n')
+                try:
+                    f.write(f'{row.f_ldes}\n')
+                except AttributeError: # No Value
+                    f.write(f'\n')
+                try:
+                    if str(row.f_unit) == "None":
+                        unit = ""
+                    else:
+                        unit = row.f_unit       
+                    f.write(f'.units {unit}\n')
+                except AttributeError: # No Value
+                    f.write(f'.units\n')
                 f.write(f'.type {row.f_dtype}*{int(row.f_precs)}\n')
                 try:
-                    f.write(f'.valu {row.f_value}\n')
-                    f.write(f'.dim {int(row.f_dim1)}, {int(row.f_dim2)}, {int(row.f_dim3)}\n')
-                    f.write(f'.pred {row.f_pred}\n\n')
+                    if str(row.f_value) == "None":
+                        valu = ""
+                    else:
+                        valu = row.f_value
+                    f.write(f'.valu {valu}\n')
                 except AttributeError: # No Value
-                    f.write(f'.dim {int(row.f_dim1)}, {int(row.f_dim2)}, {int(row.f_dim3)}\n')
-                    f.write(f'.pred {row.f_pred}\n\n')                    
+                    f.write(f'.valu\n')
+                f.write(f'.dim {int(row.f_dim1)}, {int(row.f_dim2)}, {int(row.f_dim3)}\n')
+                f.write(f'.pred {row.f_pred}\n\n')                    
                 
 # Main comparison function
-def compare_databases(db1_path, db2_path, output_file, table_name, columns_to_compare, db1_name, db2_name):
+def compare_databases(db1_path, db2_path, output_file, table_name, db1_name, db2_name):
     # Connect to both databases
     conn1 = connect_to_mdb(db1_path)
     conn2 = connect_to_mdb(db2_path)
@@ -116,7 +128,7 @@ def compare_databases(db1_path, db2_path, output_file, table_name, columns_to_co
     # Compare the specified table
     df1 = read_table(conn1, table_name)
     df2 = read_table(conn2, table_name)
-    compare_column_differences(df1, df2, table_name, columns_to_compare, output_file, db1_name, db2_name)
+    compare_column_differences(df1, df2, table_name, output_file, db1_name, db2_name)
 
     # Close the connections
     conn1.close()
@@ -127,10 +139,8 @@ def main():
     parser = argparse.ArgumentParser(description="Compare two MDB databases and write differences to a file.")
     parser.add_argument("db1", help="File name of the first MDB database")
     parser.add_argument("db2", help="File name of the second MDB database")
-    parser.add_argument("--table", help="Table name to compare (default: vars)", default="vars")
-    parser.add_argument("--columns", nargs='+', help="List of columns to compare for differences \
-                        (default:f_ptid, f_brief, f_dtype, f_precs, f_unit, f_value, f_pred, f_dim1, f_dim2, f_dim3, f_size, f_ldes)",
-                        default=["f_ptid","f_brief","f_dtype","f_precs","f_unit","f_value","f_pred","f_dim1","f_dim2","f_dim3","f_size","f_ldes"])
+    parser.add_argument("--table", help="Table name to compare", required=True)
+    # parser.add_argument("--column", help="Column to compare for differences (default: f_ptid)", default="f_ptid")
     parser.add_argument("--output", help="File to write the differences (default: differences.txt)", default="differences.txt")
 
     args = parser.parse_args()
@@ -142,15 +152,13 @@ def main():
     db1_path = os.path.join(current_dir, args.db1)
     db2_path = os.path.join(current_dir, args.db2)
     output_file = os.path.join(current_dir, args.output)
-    # print(args.columns)
-    # columns = args.columns.join(",")
-    # print(columns)
 
     # Clear the output file if it already exists
     open(output_file, 'w').close()
 
     # Compare the databases
-    compare_databases(db1_path, db2_path, output_file, args.table, args.columns, args.db1, args.db2)
+    # compare_databases(db1_path, db2_path, output_file, "vars", "f_ptid", args.db1, args.db2)
+    compare_databases(db1_path, db2_path, output_file, args.table, args.db1, args.db2)
 
 if __name__ == "__main__":
     main()
